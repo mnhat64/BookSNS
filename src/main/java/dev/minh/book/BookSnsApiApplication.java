7d732a858f38f98edef5e16b394b876c3c8cb858
Minh Nhat Phan <69678769+mnhat64@users.noreply.github.com>
2024-11-27 23:08:20 +0100

    init

---
+++ src/main/java/dev/minh/book/BookSnsApiApplication.java
@@ -0,0 +1,13 @@
+package dev.minh.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSnsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookSnsApiApplication.class, args);
+	}
+
+}
